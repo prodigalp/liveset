a9664acd80445931578d5161934df7578d6591ea
eyestrain <prodigalp@gmail.com>
2022-05-23 10:32:12 +0800

    Added Column One

--- sindex.php
+++ sindex.php
@@ -11,5 +11,9 @@
             <h2>Main Header</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt quae eaque sequi itaque voluptas! Perspiciatis doloremque exercitationem odit accusamus, distinctio consequatur voluptatibus eum error repellendus nemo culpa soluta mollitia fugiat!</p>
         </div>
+        <div class="colONe">
+            <h2>Column One</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga suscipit quas placeat illum unde saepe repellat quis, dignissimos iste. Necessitatibus eum voluptatem aperiam neque quibusdam eveniet voluptatum accusantium id! Nisi!</p>
+        </div>
     </body>
 </html>
