f8c956aa4f2af24d975228401d3c0376ab412487
eyestrain <prodigalp@gmail.com>
2022-05-23 10:23:48 +0800

    Added Boiler Plate

---
+++ sindex.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Cloning Boiler Plate</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div id="wrapper">
+            <h2>Main Header</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt quae eaque sequi itaque voluptas! Perspiciatis doloremque exercitationem odit accusamus, distinctio consequatur voluptatibus eum error repellendus nemo culpa soluta mollitia fugiat!</p>
+        </div>
+    </body>
+</html>
